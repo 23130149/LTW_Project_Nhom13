4e6c7f9dc227f9b6ae36926a2fedef27e37bb01a
ThanhPhu <236942365+23130241-hash@users.noreply.github.com>
2025-12-28 04:38:26 +0700

    Phu (#64)
    * sua lai trang admin phan danh gia
    
    * danh gia trang admin
    
    * Sua lai admin
    
    * Sua trang admin
    
    * them cai dat trang admin
    
    * them js cho admin
    
    * Ignore IntelliJ IDEA files
    
    * sua thanh jsp trang  admin
    
    * lam trang san pham
    
    * Remove target files and ignore build output
    
    * resolve
    
    * resolve conflict

--- project/src/main/controller/SignInController.java
+++ project/src/main/controller/SignInController.java
@@ -4,6 +4,19 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
+@WebServlet(name = "productController", value = "/productController")
+public class productController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductService ps = new ProductService();
+        request.getRequestDispatcher("sanpham.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
 @WebServlet(name = "SignIn", value = "/SignIn")
 public class SignInController extends HttpServlet {
 

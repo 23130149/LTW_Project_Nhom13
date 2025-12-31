6a46564fcb00cbce1ba0607c67b18b0529e55a9b
phu <23130241@st.hcmuaf.edu.vn>
2025-12-31 18:25:35 +0700

    Tiep tuc trang san pham

--- project/src/main/java/controller/RegisterController.java
+++ project/src/main/java/controller/RegisterController.java
@@ -3,6 +3,7 @@ package controller;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+
 import java.io.IOException;
 
 @WebServlet("/Register")
--- project/src/main/java/service/ProductService.java
+++ project/src/main/java/service/ProductService.java
@@ -7,8 +7,15 @@ import java.util.List;
 
 public class ProductService {
     ProductDao pdao = new ProductDao();
+
     public List<Product> getListProduct() {
         return pdao.getListProduct();
+    }
+    public List<Product> getProductsPerPage(int limit, int offset) {
+        return pdao.getProductsPerPage(limit, offset);
+    }
+    public int getTotalProducts() {
+        return pdao.getTotalProducts();
     }
     public Product getProductById(int id) {
         return pdao.getProductById(id);
